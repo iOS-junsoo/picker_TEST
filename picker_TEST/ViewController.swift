1119baa0470a20dde4a5f4a48ce827ba6377535c
kimjunsoo <rlawnstn01023@naver.com>
2021-09-17 21:21:01 +0900

    Initial Commit

---
+++ picker_TEST/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  picker_TEST
+//
+//  Created by 준수김 on 2021/09/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
